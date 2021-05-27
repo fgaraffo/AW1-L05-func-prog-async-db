43bb4ee2c2e72aa27c406d5987a1e71899c0ad58
flg <pluton3@icloud.com>
2021-05-27 18:24:56 +0200

    v1 - conversione, media

--- score-us.js
+++ score-us.js
@@ -46,13 +46,27 @@ function ExamList() {
 
     return avg;
   };
-}
+
+  this.remap = (score) => {
+    if (score >= 27) return 'A';
+    else if (score >= 24) return 'B';
+    if (score >= 19) return 'C';
+    else return 'D';
+  };
+
+  this.convertUs = () => {
+    return this.list.map( ({score, ...e}) => ( {...e, score: this.remap(score)} ));
+  };
+  //const newobj = Array.of({}, obj, (score: nuovoscore));
+};
 
 const wa1 = new Exam('01KTF', 'Web Applications I', 6, 30, false, dayjs('2021-06-01'));
+const aw1 = new Exam('01KTF', 'Applicazioni Web I', 6, 30, false, dayjs('2021-06-01'));
 const sdp = new Exam('02XXX', 'System and Device Programming', 10, 21, false, dayjs('2021-07-01'));
 
 const exams = new ExamList();
 exams.add(wa1);
+exams.add(aw1);
 exams.add(sdp);
 
 console.log("FIND");
@@ -68,4 +82,18 @@ console.log(exams.listByScore());
 console.log("AVERAGE");
 console.log(exams.average());
 
-debugger;
+console.log('*-*-*-*-*-*-*-*-*-*-*-*-*-*-')
+const usExams = exams.convertUs();
+const usScores = usExams.map( e => e.score);
+const four = usScores.filter(score => score === 'A').map( x => 4 );
+const three = usScores.filter(score => score === 'B').map( x => 3 );
+const two = usScores.filter(score => score === 'C').map( x => 2 );
+const one = usScores.filter(score => score === 'D').map( x => 1 );
+
+const gpa = [...four, ...three, ...two, ...one].reduce( (sum, score, _, arr) => sum+score/arr.length, 0);
+
+console.log(usScores);
+console.log(four, three, two, one);
+console.log(gpa);
+
+ debugger;
